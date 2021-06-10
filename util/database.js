b1f3bbcc144e254dbf433e9ad9f57b5ad6924c96
pdbrooks <PDBrooks01@outlook.com>
2021-06-10 16:12:38 -0400

    changes to .env

--- util/database.js
+++ util/database.js
@@ -1,9 +1,4 @@
 const mysql = require('mysql2')
 // const config = require('../config/config.json')
-const pool= mysql.createConnection({
-  "host": process.env.CLEARDB_DATABASE_URL,
-  "user":"b5d998ac4308b6",
-  "database":"heroku_cad49eea5459fbb",
-  "password":"81a67508"
-})
+const pool= mysql.createConnection(process.env.CLEARDB_DATABASE_URL)
 module.exports = pool.promise();
