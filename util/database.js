1155fc441a7f632fe3e833673949b5acd9d81f8c
pdbrooks <PDBrooks01@outlook.com>
2021-06-09 23:24:35 -0400

    test 6

--- util/database.js
+++ util/database.js
@@ -1,9 +1,6 @@
 const mysql = require('mysql2')
-const config = require('../config/config.json')
+// const config = require('../config/config.json')
 const pool= mysql.createPool({
-    host: config.host,
-    user: config.user,
-    database:config.database,
-    password:config.password
+  process.env.CLEARDB_DATABASE_URL
 })
 module.exports = pool.promise();
